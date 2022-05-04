12d812e0d6f69fd96f14523baaf5853f84816941
FELIPEC0D3R <102861686+FELIPEC0D3R@users.noreply.github.com>
2022-05-04 11:28:14 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,15 @@
+window.onload = function () {  
+     
+     var paragrafo = document.querySelector("#viewSessionSotorage");
+     paragrafo.textContent = localStorage.getItem('email');
+
+     setTimeout(function(){
+         document.getElementById('msg').style.display = 'block';
+     }, 5000);
+
+     setTimeout(function(){
+        document.getElementById('msg').style.display = 'none';
+      }, 5000);
+
+}
+
